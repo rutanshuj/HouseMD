cccb864bb7cc4c6a99aed50456a3cb3a59b30634
rutanshuj <rjhaveri41@gmail.com>
2017-04-14 04:07:43 +0530

    Login Made

--- app/src/main/java/com/example/admin/housemd/LoginActivity.java
+++ app/src/main/java/com/example/admin/housemd/LoginActivity.java
@@ -1,13 +1,42 @@
 package com.example.admin.housemd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText username, password;
+    Button signUp, logIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.pass);
+
+        signUp = (Button) findViewById(R.id.signUpBttn);
+        logIn = (Button) findViewById(R.id.loginBttn);
+
+        signUp.setOnClickListener(this);
+        logIn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.signUpBttn:
+                break;
+            case R.id.loginBttn:
+                Intent i = new Intent(LoginActivity.this, NavigationActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 }
